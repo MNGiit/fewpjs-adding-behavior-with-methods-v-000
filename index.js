ea536ee7b12488744ac606effd3f810daa8cf1ba
Mohammed Noori <kmanoorib@gmail.com>
2020-02-22 03:06:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ class Cat {
     return `${this.name} says meow!`;
   }
 }
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
